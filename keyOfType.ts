ac0fa1b99dfbee09233baaa4c3f923b94095fea6
TanatornZ <bank250145.bb@gmail.com>
2022-04-12 16:33:12 +0700

    spit file

--- keyOfType.ts
+++ keyOfType.ts
@@ -17,31 +17,14 @@ type P = ReturnType<typeof f>;
 
 // Indexed Access Types
 const MyArray = [
-    {name : 'Alice' , age: 14},
-    {name : 'Bob' , age: 17},
-    {name : 'Nick' , age: 24}
+  { name: "Alice", age: 14 },
+  { name: "Bob", age: 17 },
+  { name: "Nick", age: 24 },
 ];
 
-type Person = typeof MyArray[number]
-type Age = typeof MyArray[number]['age']
-
+type Person = typeof MyArray[number];
+type Age = typeof MyArray[number]["age"];
 
 //Condition type
 //   SomeType extends OtherType ? TrueType : FalseType;
 
-
-//Mapped type 
-
-type Point = {
-    x : number,
-    y : number
-}
-
-type Readonly<T> = {
-    readonly [key in keyof T]: T[key]
-}
-
-const origin: Readonly<Point> ={
-    x: 0 ,
-    y: 0
-}
