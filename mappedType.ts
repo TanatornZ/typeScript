8259942ffc2f91661a38eeaf881c89f5d959db0b
BANKIEYZ\tanat <bank250145.bb@gmail.com>
2022-04-15 21:52:13 +0700

    add mappedType

--- mappedType.ts
+++ mappedType.ts
@@ -1,67 +1,78 @@
 //Mapped type
 
 type OptionsFlags<Type> = {
-    [Property in keyof Type]: boolean;
-  };
-  // ---cut---
-  type FeatureFlags = {
-    darkMode: () => void;
-    newUserProfile: () => void;
-  };
-  
-  type FeatureOptions = OptionsFlags<FeatureFlags>;
-  
-  // remove readonly
-  type CreateMutable<Type> = {
-    -readonly [Property in keyof Type]: Type[Property];
-  };
-  
-  type LockedAccount = {
-    readonly id: string;
-    readonly name: string;
-  };
-  
-  type UnlockedAccount = CreateMutable<LockedAccount>;
-  
-  // remove optional
-  type Concrete<Type> = {
-    [Property in keyof Type]-?: Type[Property];
-  };
-  
-  type MaybeUser = {
-    id: string;
-    name?: string;
-    age?: number;
-  };
-  
-  type User = Concrete<MaybeUser>;
-  
-  //as
-  type Getters<Type> = {
-    [Property in keyof Type as `get${Capitalize<
-      string & Property
-    >}`]: () => Type[Property];
-  };
-  
-  interface Persons {
-    name: string;
-    age: number;
-    location: string;
-  }
-  
-  type LazyPerson = Getters<Persons>;
-  
-  //remove
-  // Remove the 'kind' property
-  type RemoveKindField<Type> = {
-    [Property in keyof Type as Exclude<Property, "kind">]: Type[Property];
-  };
-  
-  interface Circle {
-    kind: "circle";
-    radius: number;
-  }
-  
-  type KindlessCircle = RemoveKindField<Circle>;
-  //   ^?
-  
+  [Property in keyof Type]: boolean;
+};
+// ---cut---
+type FeatureFlags = {
+  darkMode: () => void;
+  newUserProfile: () => void;
+};
+
+type FeatureOptions = OptionsFlags<FeatureFlags>;
+
+// remove readonly
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+};
+
+type UnlockedAccount = CreateMutable<LockedAccount>;
+
+// remove optional mean  remove "?"
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+
+type User = Concrete<MaybeUser>;
+
+//as is rename of property
+type Getters<Type> = {
+  [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property];
+};
+
+interface Persons {
+  name: string;
+  age: number;
+  location: string;
+}
+
+type LazyPerson = Getters<Persons>;
+
+// is result
+
+// type LazyPerson = {
+//     getName: () => string;
+//     getAge: () => number;
+//     getLocation: () => string;
+// }
+
+//--------------------------------------------
+
+
+
+//remove
+// Remove the 'kind' property
+    type RemoveKindField<Type> = {
+      [Property in keyof Type as Exclude<Property, "kind">]: Type[Property];
+    };
+
+    interface Circle {
+      kind: "circle";
+      radius: number;
+    }
+
+    type KindlessCircle = RemoveKindField<Circle>;
+//  type KindlessCircle = {
+//     radius: number;
+// }
